2685e225a1e3302a161118967cbe1bd39415a446
Ayush Jain <ayush.jain@GALAXY.RADIXWEB.COM>
2022-06-27 18:14:33 +0530

    p-1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Greetings Of The Day, {{ name }}</h3>
+    <input v-model="name" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Ayush",
+    };
+  },
+};
+</script>
