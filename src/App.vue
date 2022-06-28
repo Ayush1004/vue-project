b4fd0a7f0c4f33566ddd84daa585a44b79b109d3
Ayush Jain <ayush.jain@GALAXY.RADIXWEB.COM>
2022-06-28 17:37:59 +0530

    p-3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <calc></calc>
+  <rectangle></rectangle>
 </template>
 
 <script>
 import home from "./components/home.vue";
 import calc from "./components/calc.vue";
-
+import HelloWorld from "./components/HelloWorld.vue";
+import rectangle from "./components/rectangle.vue";
 export default {
   data() {},
   components: {
     home,
     calc,
+    HelloWorld,
+    rectangle,
   },
 };
 </script>
---
+++ src/components/rectangle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label for="x">Enter Length: </label>
+    <input id="x" v-model.number="x" /><br />
+
+    <label for="y">Enter Breadth: </label>
+    <input id="y" v-model.number="y" /><br />
+
+    <button v-on:click="carea">Calculate Area</button>
+    <h3 v-if="area">Area: {{ area }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      area: 0,
+    };
+  },
+  methods: {
+    carea() {
+      this.area = this.x * this.y;
+    },
+  },
+};
+</script>
